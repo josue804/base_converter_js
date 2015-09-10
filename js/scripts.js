c2ead34a9baeb203a59a68b6153c360ca73c5172
Ryan Broughan & Josue Valverde <student@epicodus.com>
2015-09-10 15:30:23 -0700

    works for base up to 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,8 @@
-var wordCount = function(phrase) {
-  var wordArray = phrase.toLowerCase().split(" ");
-  var words = {};
-  var sortedWords = [];
-
-  for (var i = 0; i < wordArray.length; i += 1) {
-    var word = wordArray[i];
-    words[word] = (words[word] || 0) + 1;
+var baseConverter = function(base, number) {
+  if (number < base) {
+    return number;
   }
-
-  for(var property in words) {
-    sortedWords.push([property, words[property]]);
-  }
-
-  sortedWords.sort(function(a, b) {return b[1] - a[1]})
-
-  var html = "<ul>";
-  for (var j = 0; j < sortedWords.length; j += 1) {
-    html += "<li>" + sortedWords[j][0] + " : " + sortedWords[j][1] + "</li>";
-  }
-  html += "</ul>";
-  return html;
+  log = Math.log(number) / Math.log(base);
+  logFloor = Math.floor(log);
+  return Math.pow(10, logFloor) + baseConverter(base, number - Math.pow(base, logFloor));
 }
-
-$(document).ready(function() {
-  $("form#function").submit(function(event) {
-    var phrase = $("input#input").val();
-    var result = wordCount(phrase);
-
-    document.getElementById("result").innerHTML = result;
-
-    $("#result").show();
-    event.preventDefault();
-  });
-});
